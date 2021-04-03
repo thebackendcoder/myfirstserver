4c97b1ce0ec9d2f56ae97146fb0628ef192b3890
Kashif Kalam <Kashif.Kalam@viacom18.com>
2021-04-03 10:30:42 +0530

    this is my first server project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res){
+
+    res.send("welcome to the home page")
+})
+
+app.listen('3000', function(){
+console.log("i am listening at port 3000")
+});
+
+
