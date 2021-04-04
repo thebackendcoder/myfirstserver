45f1195f91da8ee085bbd532adbee0f36a902530
Kashif Kalam <Kashif.Kalam@viacom18.com>
2021-04-04 12:24:04 +0530

    pushing another branch

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 app.get('/asglb', function(req, res){
     res.send("testing auto scaling and load balancer")
 })
-
 const port = process.env.port || 3000;
 app.listen(port, function(){
 console.log("i am listening at port 3000")
