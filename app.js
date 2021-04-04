82f99b19af59bb4a775933222a686b3add988865
Kashif Kalam <Kashif.Kalam@viacom18.com>
2021-04-04 11:10:18 +0530

    added port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', function(req, res){
     res.send("hey baby welcome to the home page ")
 })
 
+const port = process.env.port || 3000;
 app.listen('3000', function(){
 console.log("i am listening at port 3000")
 });
